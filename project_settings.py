b1641302f7e001a5d6e3ecb9058f7ed3a77f8eb3
Mark Muetzelfeldt <markmuetz@gmail.com>
2013-11-29 16:48:07 +0000

    Add an example data file location to settings.

--- project_settings.py
+++ project_settings.py
@@ -5,7 +5,10 @@ if socket.gethostname() == 'breakeven-pangolin':
     DATA_DIR= '/media/SAMSUNG/geogg122_data'
 else:
     print("DATA_DIR needs to be configured on this computer")
-    #DATA_DIR= 'data_files'
+    # You can just comment out the next line to have it create a 
+    # folder in your home dir. Warning, it can use quite a lot of
+    # disk space (~1.5GB for one year's worth of data.
+    #DATA_DIR= '~/geogg122_data'
     raise Exception('DATA_DIR needs to be configured on this computer')
 
 TILE = 'h09v05'
