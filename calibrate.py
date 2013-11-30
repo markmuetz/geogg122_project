1397d11aa168534e80298452c54aa012a8a41440
Mark Muetzelfeldt <Mark Muetzelfeldt>
2013-11-30 14:09:32 +0000

    Modify calibrate so as it can handle 2010 data.

--- calibrate.py
+++ calibrate.py
@@ -30,15 +30,16 @@ def calibrate_model(data, start_date, end_date):
     temperature_for_year = temperature[date_mask]
     snow_prop_for_year = snow_data['COMBINED_total_snow'][date_mask]
 
-    model_data = {'temp': temperature_for_year, 'snowprop': snow_prop_for_year }
+    model_data = {'temp': temperature_for_year[:len(discharge_for_year)], 
+	          'snowprop': snow_prop_for_year[:len(discharge_for_year)] }
 
     p_guess = [8.4, 0.000045, 0.95]
     p_est = optimize.fmin(obj, p_guess, args=(model_data, discharge_for_year), maxiter=10000)
     print p_est
 
     plt_dates = matplotlib.dates.date2num(dates[date_mask])
-    plt.plot_date(plt_dates, discharge_for_year, 'k', label='observed')
-    plt.plot_date(plt_dates, func(p_est, model_data), 'k--', label='modelled')
+    plt.plot_date(plt_dates[:len(discharge_for_year)], discharge_for_year, 'k', label='observed')
+    plt.plot_date(plt_dates[:len(discharge_for_year)], func(p_est, model_data), 'k--', label='modelled')
     plt.legend(loc='best')
     plt.show()
     return p_est
