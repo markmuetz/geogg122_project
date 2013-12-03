a0806478e9bc5b49f2564c974018893ed3dfab9e
Mark Muetzelfeldt <markmuetz@gmail.com>
2013-12-03 12:23:08 +0000

    Changed comment about setting DATA_DIR

--- project_settings.py
+++ project_settings.py
@@ -10,9 +10,10 @@ elif (len(hostname.split('.')) != 0 and
 else:
     print("DATA_DIR needs to be configured on this computer")
     raise Exception('DATA_DIR needs to be configured on this computer\nPlease edit project_settings.py')
-    # You can just comment out the next line to have it create a 
-    # folder in your home dir. Warning, it can use quite a lot of
-    # disk space (~1.5GB for one year's worth of data.
+    # You can just comment out the previous lines and uncomment out the 
+    # next line to have it create a folder in your home dir. 
+    # Warning, it can use quite a lot of disk space 
+    # (~1.5GB for one year's worth of data.)
     # DATA_DIR= '~/geogg122_data'
 
 TILE = 'h09v05'
